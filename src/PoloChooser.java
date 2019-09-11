77f36e84ea0a67001d04dd95499d45d8113d1995
jonathan.egan <jonathan.egan@MU-C1-12.mullen.pvt.k12.co.us>
2019-09-11 12:00:29 -0600

    commit

--- src/PoloChooser.java
+++ src/PoloChooser.java
@@ -1,4 +1,6 @@
+import java.util.Random;
 import java.util.Scanner;
+
 public class PoloChooser 
 {
 	public static void main(String[] args) 
@@ -8,9 +10,14 @@ public class PoloChooser
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("Here are the polo you can wear.");
 		
+		
 		for(String polo : myArray)
 		{
 			System.out.println(polo);
 		}
+		
+		Random r = new Random();
+		int randomNumber=r.nextInt(myArray.length);
+        System.out.println("You are going to wear " + myArray[randomNumber] + ".");
 	}
 }
